fbd1ff45d95a20e7cb9c666e1b4b621c687c4b81
LuizQuirino <luizfpq@gmail.com>
2021-01-02 18:20:09 -0300

    add project idea

---
+++ prestaki-backend/project.php
@@ -0,0 +1,11 @@
+<?php
+    echo "projeto prestaki.";
+
+    /**
+     * Projeto criado para estudos e posteriormente 
+     * uso em aplicativo foca em encontrar concursos
+     * e avisar o concurseiro sobre os dados importantes e vagas
+     */ 
+
+     $source = "https://www.octoparse.com/blog/creating-a-simple-web-crawler-in-php-you-need-an-automation-one";
+?>
