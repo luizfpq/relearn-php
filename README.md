0437c3fe8b73b5f2db8db9ad25c807fe95345f47
Luiz Quirino <luizfpq@gmail.com>
2021-01-04 01:07:26 -0300

    create init template from cod3r

--- README.md
+++ README.md
@@ -4,16 +4,17 @@
 # Projeto php-relearn
 
 ## Descrição
-Estudos de PHP7 e PHP8 com foco em reciclagem e reforço de conhecimento, levando em conta que fiquei muito tempo sem estudar a linguagem.
+Estudos de PHP7 e PHP8 com foco em reciclagem e reforço de conhecimento, levando em conta que fiquei muito tempo sem estudar a fundo a linguagem.
 
 ## Justificativas
-Por vários anos programei em PHP sem me importar com o mundo ao redor, produzia codigo e tentava entender o porquê de algumas coisas funcionarem e outras não.
+Por vários anos, programei em PHP sem me importar com o mundo ao redor, produzia código e tentava entender o porquê de algumas coisas funcionarem e outras não.
 
-Sendo assim comecei a enfrentar algumas dificuldades por volta dos anos 2010, onde fiz uma boa reciclagem, criei alguns projetos pessoais, vendi algumas aplicações, contudo, novamente entrei num ciclo de mim mesmo e voltei a trabalhar com meu conhecimento, as documentações e nada mais.
+Sendo assim comecei a enfrentar algumas dificuldades por volta dos anos 2010, onde fiz uma boa reciclagem, criei alguns projetos pessoais, vendi algumas aplicações, contudo, novamente entrei num ciclo de produção superficial sem estudos mais aprofundados e acompanhamento da evolução da linguagem, em critérios de arquitetura, sintaxe, segurança, desempenho, etc.
 
-Chegamos novamente a um ponto de mudança, onde vários projetos precisam de conhecimento lapidado, e a maturidade me permitiu aceitar que estudo e conhecimentos tem de ser adiquiridos constantemente.
+Chegamos novamente a um ponto de mudança, onde vários projetos precisam de conhecimento lapidado, e a maturidade me permitiu aceitar que estudo e conhecimentos tem de ser adquiridos constantemente.
 
-Portanto, este repositório vem para um espaço novo em minha mente, a do constante reapridizado, do foco em polir minhas habilidades e buscar novos níveis de qualidade.
+Portanto, este repositório vem para um espaço novo em minha mente, a do constante reaprendizado, do foco em polir minhas habilidades e buscar novos níveis de qualidade.
+
 
 
 
---
+++ cod3r-udemy/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP 7 &middot; COD3R</title>
+
+    <!-- fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <h1>Curso PHP 7 &middot; COD3R</h1>
+        <h2>Indice dos Exercícios</h2>
+
+    </header>
+    <main class="main">
+        <div class="content">
+            <nav class="modules">
+                <div class="module color1">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color2">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color3">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color4">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color5">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color6">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color7">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color8">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+                <div class="module color9">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="footer">
+        Cod3r & alunos &copy; <?php echo date('Y') ?>
+    </footer>
+
+</body>
+
+</html>
